f51b4ea7c0dc217b5737223f8c3b0c02b0b58502
efpcode <efpjobhunt@gmail.com>
2022-10-16 21:14:51 +0200

    Removed everything that should not be there anymore

--- fourinrow/tests/test_view.py
+++ fourinrow/tests/test_view.py
@@ -1,33 +1,40 @@
 # -*- coding: utf-8 -*-
-from fourinrow.fourinrow import board_tokens, create_board
-from fourinrow.fourinrow import PlayerTokens, show_board
+import pytest
+from fourinrow.fourinrow import BoardValues, PlayerTokens, show_board, view_board_tokens
 
 
-def test_display_empty_token() -> None:
-    board = create_board(2, 2)
-    for row, _ in enumerate(board):
+@pytest.fixture
+def board():
+    return BoardValues(6, 7)
+
+
+@pytest.fixture
+def small_board():
+    return BoardValues(1, 1)
+
+
+def test_display_empty_token(board) -> None:
+    for row, _ in enumerate(board.board):
         for column, _ in enumerate(_):
-            token = board_tokens(board[row][column])
+            token = view_board_tokens(board.board[row][column])
             assert token == PlayerTokens.NO_PLAYER.value
 
 
 def test_display_token_defaults() -> None:
     tokens = PlayerTokens.get_all_player_tokens()
     for idx, player in enumerate(PlayerTokens):
-        token = board_tokens(player.value)
+        token = view_board_tokens(player.value)
         assert token == tokens[idx]
 
 
-def test_display_token_edit_value() -> None:
-    board = create_board(1, 1)
-    board[0][0] = 1
-    token = board_tokens(board[0][0])
-    assert token == 1
+def test_display_token_edit_value(board) -> None:
+    board.set_board_value((0, 0), PlayerTokens.CPU.value)
+    token = view_board_tokens(board.board[0][0])
+    assert token is PlayerTokens.CPU.value
 
 
-def test_display_board(capsys) -> None:
-    board = create_board(1, 1)
-    show_board(board)
+def test_display_board(capsys, small_board) -> None:
+    show_board(small_board.board)
     board_img = capsys.readouterr()
     expected_layout = "1 ['☐']\n    1\n"
     assert board_img.out == expected_layout
